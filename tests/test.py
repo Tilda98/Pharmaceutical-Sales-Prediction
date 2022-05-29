7c81316a86ae6471b71c3c5b8937f588f3bd62e7
Matilda <102175065+Tilda98@users.noreply.github.com>
2022-05-29 10:11:45 +0300

    Setup

--- tests/test.py
+++ tests/test.py
@@ -4,3 +4,11 @@ import unittest
 import pandas as pd
 import pandas.api.types as ptypes
 from pandas.api import types
+
+sys.path.append('..')
+from scripts.data_cleaning import DataCleaner
+
+class TestDataCleaner(unittest.TestCase):
+
+    def setUp(self) -> pd.DataFrame:
+        self.cleaner = DataCleaner()
